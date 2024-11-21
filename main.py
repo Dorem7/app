b77b56d86094507b1b054b0389d57c8194f9b8d0
Sufiyan Kazi <sb15903@student.samk.fi>
2024-11-21 15:45:36 +0200

    imported sq

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from addition import add_two_numbers
+from Sq_Root import sq_root
 
 print("Using addition")
 x = add_two_numbers(1, 2)
 print("1 + 2 = " + x)
+y= sq_root(2)
+print("The Sq of is:" + y)
